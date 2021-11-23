7c3a0cb2786e3169b3fdbaf652c828eb43b1c682
Artem <gorbulya@gmail.com>
2021-11-23 20:26:56 +0200

    Signed-off-by: Artem <gorbulya@gmail.com>

---
+++ src/components/app/app.js
@@ -0,0 +1,130 @@
+import { Component } from 'react';
+
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'Artem G.', salary: 800, increase: false, rise: true, id: 1},
+                {name: 'Nataly S.', salary: 1000, increase: true, rise: false, id: 2},
+                {name: 'Mike R.', salary: 2000, increase: false, rise: false, id: 3},
+                {name: 'Stive W.', salary: 1500, increase: false, rise: false, id: 4},
+                {name: 'Antonio L.', salary: 1300, increase: false, rise: false, id: 5}
+            ],
+            term: '',
+            filter: ''
+        }
+        this.maxId = 4;
+    }
+
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false,
+            rise: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
+
+    searchEmp=(items, term) => {
+        if (term.length === 0){
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term})
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item=> item.rise)
+            case 'moreThen1000': 
+                return items.filter(item => item.salary>1000)
+            default:
+                return items        
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
+
+    onChangeSalary = (id, salary) => {
+
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, salary}
+                }
+                return item;
+            })
+        }))
+    }
+
+    render() {
+        const {data, term, filter} = this.state;
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+
+        return (
+            <div className="app">
+                <AppInfo employees={employees} increased={increased}/>
+    
+                <div className="search-panel">
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
+                </div>
+                
+                <EmployeesList 
+                    data={visibleData}
+                    onDelete={this.deleteItem}
+                    onToggleProp={this.onToggleProp}
+                    onChangeSalary={this.onChangeSalary}/>
+                <EmployeesAddForm onAdd={this.addItem}/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/employees-list-item/employees-list-item.js
@@ -0,0 +1,55 @@
+import { Component } from 'react';
+import './employees-list-item.css';
+
+class EmployeesListItem extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            salary: this.props.salary
+        }
+    }
+
+    onChangeSalary = (e) =>{
+        const salary = e.target.value;
+        this.setState({salary});
+        this.props.onChangeSalary(this.props.id, salary);
+        
+    }
+
+    render(){
+
+        const {name, onDelete, onToggleProp, increase, rise} = this.props;
+
+        let classNames = "list-group-item d-flex justify-content-between";
+        if (increase) {
+            classNames += ' increase';
+        }
+        if (rise) {
+            classNames += ' like';
+        }
+
+        return (
+            <li className={classNames}>
+                <span className="list-group-item-label" onClick={onToggleProp} data-toggle="rise">{name}</span>
+                <input type="text" className="list-group-item-input" value={this.state.salary} onChange={this.onChangeSalary}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                        className="btn-cookie btn-sm "
+                        onClick={onToggleProp}
+                        data-toggle="increase">
+                        <i className="fas fa-cookie"></i>
+                    </button>
+    
+                    <button type="button"
+                            className="btn-trash btn-sm "
+                            onClick={onDelete}>
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
+}
+
+export default EmployeesListItem;
